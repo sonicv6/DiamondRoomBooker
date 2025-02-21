c7568f62cde500245f554ac86687973da899ed84
Dylan Manning <dmanning1@sheffield.ac.uk>
2025-02-21 03:00:37 +0000

    Create ScheduleService.cs

---
+++ LibCalBooker/ScheduleService.cs
@@ -0,0 +1,25 @@
+﻿using LibCalBooker.Data;
+using LibCalBooker.LibCal;
+
+namespace LibCalBooker
+{
+	public class ScheduleService
+	{
+		private LibCalContext ctx;
+
+		public ScheduleService(LibCalContext c)
+		{
+			ctx = c;
+		}
+
+		public async Task CreateScheduledBookings()
+		{
+			int attempts = 0;
+			while (attempts++ < 8)
+			{
+				await LibCalSession.BookScheduledRooms(ctx);
+				await Task.Delay(30 * 1000);
+			}
+		}
+	}
+}
